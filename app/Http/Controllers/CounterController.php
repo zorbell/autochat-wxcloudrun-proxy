33194e59499cb872ec7e51009605a36e601f394d
erick <zorbell@hotmail.com>
2023-03-22 02:43:58 -0700

    -

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -21,9 +21,26 @@ class CounterController extends Controller
 {
     public function proxy(){
         
-        error_log(json_encode(file_get_contents('php://input'), JSON_UNESCAPED_UNICODE).PHP_EOL);
-        return response(0);
+        error_log(json_encode(file_get_contents('php://input'), JSON_UNESCAPED_UNICODE));
+        $result = posturl("https://chator.wrekee.com/connector/wechat/messager/fromproxy", file_get_contents('php://input'));
+        return response("success");
 
+    }
+
+    private function posturl($url,$data){
+        $data  = json_encode($data, JSON_UNESCAPED_UNICODE);    
+        $headerArray =array("Content-type:application/json;charset='utf-8'","Accept:application/json");
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl,CURLOPT_HTTPHEADER,$headerArray);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($output，true);
     }
     
     /**
